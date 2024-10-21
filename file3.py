e209897b69ffa77e6425911de20e9c7588e90501
pyb0b <mohamad.a.ibrahim2000@hotmail.com>
2024-10-21 22:45:26 +0300

    answer to ex3 8 and 9

--- file3.py
+++ file3.py
@@ -1,7 +1,7 @@
 def f(x):
     result = x*x
     i = 1
-    while i <= x:
+    while i <= x: #this is the condition
         result = result + i
         i = i + 1
     return result
--- file4.py
+++ file4.py
@@ -1,1 +1,4 @@
 #git restore file3.py to discard changes
+#revert creates a new commit automatically and 
+#you can revert what you revert
+#restore just discard what you did
